f180a5d0633d942fb660601f6a75621d8ac8bf9b
Cacula10 <flaviodesouzamarques@gmail.com>
2019-10-15 15:23:17 -0300

    Removido variavel de teste

--- SAJ.py
+++ SAJ.py
@@ -6,16 +6,14 @@ conn = pyodbc.connect(
     "Database=SPJHML;"
     "uid=saj;pwd=nltrecRephlcrA"
 )
-
 cursor = conn.cursor()
 proc = str( input( 'Informe o numero do processo: ' ) )
 proc = '%' + proc + '%'
 cursor.execute(
     "SELECT A.NUNAOFORMATADO, A.CDUSUINCLUSAO, B.CDPROCESSO, B.NUPROCESSO FROM SAJ.ESPJPROCOUTROSNUM AS A JOIN SAJ.ESPJPROCESSO AS B ON A.CDPROCESSO = B.CDPROCESSO WHERE A.NUNAOFORMATADO LIKE ?",
-    proc )
+    proc)
 
 for row in cursor:
-    print( row )
-print( proc )
+    print(row)
 
-a = str( input( '=' ) )
+a = str(input('='))
