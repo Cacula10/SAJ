2fff15c034038378fcdaa731f4aec4a25a7adfad
Cacula10 <flaviodesouzamarques@gmail.com>
2019-10-15 16:26:39 -0300

    Adicioando valores em lista e usando o If

--- SAJ.py
+++ SAJ.py
@@ -1,19 +1,47 @@
 import pyodbc
 
+# Repositório de variáveis
+lista=[]
+#
 conn = pyodbc.connect(
     "Driver={SQL Server Native Client 11.0};"
     "Server=10.100.188.129\ISAJ01;"
     "Database=SPJHML;"
     "uid=saj;pwd=nltrecRephlcrA"
 )
-cursor = conn.cursor()
-proc = str( input( 'Informe o numero do processo: ' ) )
-proc = '%' + proc + '%'
-cursor.execute(
-    "SELECT A.NUNAOFORMATADO, A.CDUSUINCLUSAO, B.CDPROCESSO, B.NUPROCESSO FROM SAJ.ESPJPROCOUTROSNUM AS A JOIN SAJ.ESPJPROCESSO AS B ON A.CDPROCESSO = B.CDPROCESSO WHERE A.NUNAOFORMATADO LIKE ?",
-    proc)
+# Criação dos dois cursores para busca de Outros Numeros e Não formatado
+cursor_out = conn.cursor()
+cursor_nformat = conn.cursor()
 
-for row in cursor:
-    print(row)
+# Guardando as variaveis
+outnum = str(input('Digite um numero para busca: '))
 
-a = str(input('='))
+
+#Concatenando com % para utilizar o like
+outnum = '%' + outnum + '%'
+#nformat = '%' + nformat + '%'
+
+# Select do Banco
+cursor_out.execute(" SELECT A.NUNAOFORMATADO, A.CDUSUINCLUSAO, B.CDPROCESSO, B.NUPROCESSO FROM SAJ.ESPJPROCOUTROSNUM AS A JOIN SAJ.ESPJPROCESSO AS B ON A.CDPROCESSO = B.CDPROCESSO WHERE A.NUNAOFORMATADO LIKE ?",outnum)
+
+# Append na lista
+for row in cursor_out:
+    lista.append(row)
+
+# exibindo os processos encontrados
+if len(lista) > 20:
+    continuar= str(input('O sistema encontrou mais que 20 processos com esse número você deseja exibi-los ? '))[0].upper()
+    if continuar == 'S':
+        print(lista)
+        print()
+
+continuar= str(input('Deseja continuar procurando ?'))
+
+if continuar == 'N':
+    print('FIM')
+else:
+    nformat = str(input('Digite um numero para uma nova busca'))
+
+
+
+print('FIM')
