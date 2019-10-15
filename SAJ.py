13f0d4c03b7abbd5c6ac747dba8d2863baba18bc
Cacula10 <flaviodesouzamarques@gmail.com>
2019-10-15 20:56:09 -0300

    Corrigindo váriaveis e continuando projeto

--- SAJ.py
+++ SAJ.py
@@ -1,7 +1,7 @@
 import pyodbc
 # Repositório de variáveis
-lista=[]
-lista2=[]
+outros_num=[]
+nunaoformato=[]
 # conexão a base de dados
 conn = pyodbc.connect(
     "Driver={SQL Server Native Client 11.0};"
@@ -12,26 +12,33 @@ conn = pyodbc.connect(
 # Criação do cursor para busca do processo
 cursor = conn.cursor()
 cursor2 = conn.cursor()
+
 # Guardando as variaveis e concatenando para uso do like no banco
 proc = str(input('Digite um numero para busca: '))
 proc = '%' + proc + '%'
 
-# Select do Banco
+# Select's do Banco
 cursor.execute("SELECT A.NUNAOFORMATADO, A.CDUSUINCLUSAO, B.CDPROCESSO, B.NUPROCESSO FROM SAJ.ESPJPROCOUTROSNUM AS A JOIN SAJ.ESPJPROCESSO AS B ON A.CDPROCESSO = B.CDPROCESSO WHERE A.NUNAOFORMATADO LIKE ?",proc)
 for row in cursor:
-    lista.append(row)
+    outros_num.append(row)
 cursor2.execute("SELECT * FROM ESPJPROCESSO WHERE NUNAOFORMATADO LIKE ?",proc)
 for row in cursor2:
-    lista2.append(row)
+    nunaoformato.append(row)
 
 print()
 print("Segue abaixo os processos encontrados")
 print()
-print(lista)
+# Exibição ao usuário
+print(f'Foram encontrado {len(outros_num)} processos')
+print()
+print('Cod')
+for row in outros_num:
+    print(row[0])
+print()
 continua = str(input("Deseja continuar? [S/N]"))[0].upper()
 if continua == 'S':
     print("Segue abaixo o restante dos processos")
     print()
-    print(lista2)
+    print(nunaoformato)
 else:
     print("FIM")
