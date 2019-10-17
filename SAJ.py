bcd29ca5574a4298307c67b5e69607abf03a7504
Cacula10 <flaviodesouzamarques@gmail.com>
2019-10-17 19:18:47 -0300

    # Utilizar o enumerate para concluir isso

--- SAJ.py
+++ SAJ.py
@@ -1,17 +1,15 @@
 import pyodbc
-import time
 
 # Repositório de variáveis
-outros_num=[]
-nunaoformato=[]
+
 cont = 0
 l_geral = [[], [], []]
-
 
 cores = {'limpa':'\033[m',
          'azul':'\033[34m',
          'amarelo':'\033[33m',
          'preto_branco':'\033[7;33m'}
+
 # conexão a base de dados
 conn = pyodbc.connect(
     "Driver={SQL Server Native Client 11.0};"
@@ -23,18 +21,18 @@ conn = pyodbc.connect(
 cursor = conn.cursor()
 
 # Guardando as variaveis e concatenando para uso do like no banco
-proc = str(input('Digite um numero para busca: '))
-proc = '%' + proc + '%'
-time.sleep(0.5)
+num = str(input('Digite um numero para busca: '))
+num = '%' + num + '%'
 
 # Select's do Banco
-cursor.execute("SELECT CDUSUINCLUSAO, NUNAOFORMATADO, CAST(NUPROCESSO AS VARCHAR) AS PROCESSO FROM ESPJPROCESSO WHERE NUPROCESSO LIKE ? OR NUNAOFORMATADO LIKE ?",proc,proc)
+cursor.execute("SELECT CDUSUINCLUSAO, NUNAOFORMATADO, CAST(NUPROCESSO AS VARCHAR) AS PROCESSO "
+               "FROM ESPJPROCESSO WHERE NUPROCESSO LIKE ? OR NUNAOFORMATADO LIKE ?", num, num)
+
 for row in cursor:
     l_geral[0].append(row[0].rstrip())
     l_geral[1].append(row[1])
     l_geral[2].append(row[2])
     cont += 1
 
-print(l_geral)
-
-
+print(len(l_geral[0]))
+# Utilizar o enumerate para concluir isso
