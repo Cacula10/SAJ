0cc7d9f5978767772d03d8507b573af22e2e1eba
Cacula10 <flaviodesouzamarques@gmail.com>
2019-10-22 16:42:24 -0300

    Concluido a lista e Dicionario {usuário} chave de busca

--- SAJ_v1.py
+++ SAJ_v1.py
@@ -1,9 +1,10 @@
 import pyodbc
-
+from collections import defaultdict
 # Repositório de variáveis
 
 cont = 0
-l_geral = [[], [], []]
+lista_SQL = []
+dicionario_proc = defaultdict(list)
 
 cores = {'limpa':'\033[m',
          'azul':'\033[34m',
@@ -29,9 +30,20 @@ cursor.execute("SELECT CDUSUINCLUSAO, NUNAOFORMATADO, CAST(NUPROCESSO AS VARCHAR
                "FROM ESPJPROCESSO WHERE NUPROCESSO LIKE ? OR NUNAOFORMATADO LIKE ?", num, num)
 
 for row in cursor:
-    l_geral[0].append(row[0].rstrip())
-    l_geral[1].append(row[1])
-    l_geral[2].append(row[2])
+    lista_SQL.append(row[0].rstrip())
+    lista_SQL.append(row[1])
+    lista_SQL.append(row[2])
     cont += 1
 
-print(l_geral)
+splited_list = int(len(lista_SQL)/3)
+splited = []
+len_lista_SQL = len(lista_SQL)
+for i in range(splited_list):
+    start = int(i*len_lista_SQL/splited_list)
+    end = int((i+1) * len_lista_SQL/splited_list)
+    splited.append(lista_SQL[start:end])
+
+for usuario, numero, processo in splited:
+    dicionario_proc[usuario].append((numero, processo))
+
+print(dicionario_proc['3039753'])
