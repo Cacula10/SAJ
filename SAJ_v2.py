dfaf1e28889fcc410ac121de7c607dee63b9baaa
flavio.marques <flaviodesouzamarques@gmail.com>
2019-11-12 13:20:43 -0200

    Correção

--- SAJ_v2.py
+++ SAJ_v2.py
@@ -1,5 +1,4 @@
 import pyodbc
-
 lista_SQL = []
 
 conn = pyodbc.connect(
@@ -8,29 +7,26 @@ conn = pyodbc.connect(
     "Database=SPJUSP;"
     "uid=saj;pwd=nltrecRephlcrA"
 )
-
 cursor = conn.cursor()
 
 num = str(input('Digite um numero para busca: '))
 num = '%' + num + '%'
 
 cursor.execute("SELECT CAST(A.NUPROCESSO AS VARCHAR), A.NUNAOFORMATADO, B.NUNAOFORMATADO"
-               " FROM ESPJPROCESSO AS A "
-               "LEFT JOIN ESPJPROCOUTROSNUM AS B "
-               "ON A.CDPROCESSO = B.CDPROCESSO "
-               "WHERE A.NUPROCESSO LIKE ? OR A.NUNAOFORMATADO LIKE ? OR B.NUNAOFORMATADO LIKE ? ", num, num, num)
+                   " FROM ESPJPROCESSO AS A "
+                   "LEFT JOIN ESPJPROCOUTROSNUM AS B "
+                   "ON A.CDPROCESSO = B.CDPROCESSO "
+                   "WHERE A.NUPROCESSO LIKE ? OR A.NUNAOFORMATADO LIKE ? OR B.NUNAOFORMATADO LIKE ? ", num, num, num)
 
 for row in cursor:
     lista_SQL.append(row[0])
     lista_SQL.append(row[1])
     lista_SQL.append(row[2])
 
-
 print('>=' * 35)
 print('{:^70}'.format('CONFIRA A LISTAGEM'))
 print('>=' * 35)
 
 print(lista_SQL)
-print(len(lista_SQL))
 
 a = input('...')
